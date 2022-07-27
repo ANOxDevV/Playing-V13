4a4d510c11ffdc1e297d7018606ebee13784ac78
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-07-28 00:54:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('GG MEN')
-})
-
-app.listen(3000)
-
 const { Client } = require('discord.js');
 
 client.login("OTM3MDYzMjM1NjEyNDUwODI3.G7sAK1.-PTvbXKb2-MutLp4TL9PQHrdHfwV1tlDFbtul0")
