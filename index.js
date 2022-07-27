5f9f6b88bb246553ee39f928fe4c840940e1755e
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-07-28 00:20:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const client = new Client({ partials: ["CHANNEL", "MESSAGES", "GUILD_MEMBERS", "DIRECT_MESSAGES"], intents: 32767 });
 
-client.on("ready", () => {
-  console.log(`Online In Servers`);
-  let statuses = [``];
-  setInterval(function() {
-    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    client.user.setActivity(PLAYING, {
-      type: "PLAYING",
-      url: "https://www.twitch.tv/faith"
-    });
-  }, 2000);
-});
 
 
 client.login("OTM3MDYzMjM1NjEyNDUwODI3.G7sAK1.-PTvbXKb2-MutLp4TL9PQHrdHfwV1tlDFbtul0")
