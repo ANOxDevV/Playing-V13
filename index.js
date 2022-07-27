86d642bb536f5f97c78e59b6bff4ef418e1ea559
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-07-28 00:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const client = new Client({ partials: ["CHANNEL", "MESSAGES", "GUILD_MEMBERS", "DIRECT_MESSAGES"], intents: 32767 });
-
 
 
 client.login("OTM3MDYzMjM1NjEyNDUwODI3.G7sAK1.-PTvbXKb2-MutLp4TL9PQHrdHfwV1tlDFbtul0")
