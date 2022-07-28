c3ded0c86c6938071492ff84fe6462dd0fcdf8b5
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-07-28 11:57:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const { Client } = require('discord.js');
+const { Client, Collection } = require("discord.js");
 
-client.login("MTAwMTk3MTIwMzQ3MDI3NDY1MQ.GtYLyv.OoVE8GIJjhoa6D6qJWdE5_gxObyj-6E25bCgj8")
+const client = new Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [
+    "GUILDS",
+    "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+});
+module.exports = client;
+client.commands = new Collection();
+
+
+client.login("MTAwMTk3MTIwMzQ3MDI3NDY1MQ.Gjpj2e.OEMbdCgSvTkGw6NBBClEggU20KpmoBroavxWu4");
